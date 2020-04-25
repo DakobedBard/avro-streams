51d3924c7e9a3f844e60a3245d2d950c74f5376f
mddarr <mddarr@gmail.com>
2020-04-24 23:12:02 -0700

    I am getting a different error than last time

--- order-service/src/main/java/org/mddarr/orders/resources/OrderController.java
+++ order-service/src/main/java/org/mddarr/orders/resources/OrderController.java
@@ -1,34 +1,22 @@
 package org.mddarr.orders.resources;
 
 import org.mddarr.orders.event.AvroProducer;
-import org.mddarr.products.Event1;
 import org.mddarr.products.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
-
 @RestController
-@RequestMapping(value = "/user")
-public class UserResource {
-
+public class OrderController {
 
     private final AvroProducer producer;
 
     @Autowired
-    UserResource(AvroProducer producer) {
+    OrderController(AvroProducer producer) {
         this.producer = producer;
-    }
-
-    @PostMapping(value = "/publish")
-    public String sendMessageToKafkaTopic(@RequestParam("name") String name, @RequestParam("value") String value) {
-        this.producer.sendEvent1(new Event1(name, value));
-        return "hey";
-//        this.producer.sendEvent1();
     }
 
     @PostMapping("/orders/")
@@ -40,8 +28,5 @@ public class UserResource {
         this.producer.sendOrder(order);
         return order;
     }
-
-
-
 
 }
