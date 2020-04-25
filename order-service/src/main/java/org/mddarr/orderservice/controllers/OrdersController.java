f387a967bd57782de1312ea98e13898382ffc62e
mddarr <mddarr@gmail.com>
2020-04-24 20:19:49 -0700

    Add list parameters for product IDs and quantities of purchased item to to the post endpoint.  Was able to submit this request in postman w comma seperated lists as a parameter, no monkeying around w/ raw body

--- order-service/src/main/java/org/mddarr/orderservice/controllers/OrdersController.java
+++ order-service/src/main/java/org/mddarr/orderservice/controllers/OrdersController.java
@@ -2,18 +2,27 @@ package org.mddarr.orderservice.controllers;
 
 
 import org.mddarr.orderservice.resources.OrderBean;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.mddarr.products.Order;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class OrdersController {
 
-//    @RequestMapping("/product/idx")
-//    public OrderBean product(@RequestParam(value="pid") Long pid, @RequestParam(value="cid") Long cid,
-//                             @RequestParam(value="quantity") Long quantity, @RequestParam(value="price") Long price) {
-//        OrderBean order = new OrderBean(cid, pid, quantity, )
-//
-//    }
+    @PostMapping("/orders/")
+    public String product(@RequestParam(value="products") List<Long> products, @RequestParam(value="quantities") List<Long> quantities,
+        @RequestParam(value="customerID")  Long cid, @RequestParam(value="price") Long price)
+    {
+        return "The length is " + quantities.size();
+    }
+
+        // @RequestParam(value="quantites") List<Long> quantities , @RequestParam(value="cid") Long cid, @RequestParam(value="price") Long price) {
+//        OrderBean order = new OrderBean(cid, products, quantities,"CREATED", price );
+//        Order a;
+//        return order;
+
+
 
 }
--- order-service/src/main/java/org/mddarr/orderservice/resources/OrderBean.java
+++ order-service/src/main/java/org/mddarr/orderservice/resources/OrderBean.java
@@ -1,12 +1,14 @@
 package org.mddarr.orderservice.resources;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class OrderBean {
     private Long orderID;
     private Long customerID;
-    private Long productID;
-    private Long quantity;
+    private List<Long> productIDs;
+    private List<Long> quantites;
     private String state;
     private Long price;
 
@@ -19,52 +21,55 @@ public class OrderBean {
     }
 
     @Override
-    public String toString() {
-        return "OrderBean{" +
-                "customerID=" + customerID +
-                ", productID=" + productID +
-                ", quantity=" + quantity +
-                ", state='" + state + '\'' +
-                ", price=" + price +
-                '}';
-    }
-
-    public OrderBean(Long customerID, Long productID, Long quantity, String state, Long price) {
-        this.customerID = customerID;
-        this.productID = productID;
-        this.quantity = quantity;
-        this.state = state;
-        this.price = price;
-    }
-
-    @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof OrderBean)) return false;
         OrderBean orderBean = (OrderBean) o;
-        return Objects.equals(customerID, orderBean.customerID) &&
-                Objects.equals(productID, orderBean.productID) &&
-                Objects.equals(quantity, orderBean.quantity) &&
+        return Objects.equals(orderID, orderBean.orderID) &&
+                Objects.equals(customerID, orderBean.customerID) &&
+                Objects.equals(productIDs, orderBean.productIDs) &&
+                Objects.equals(quantites, orderBean.quantites) &&
                 Objects.equals(state, orderBean.state) &&
                 Objects.equals(price, orderBean.price);
     }
 
-
     @Override
     public int hashCode() {
-        return Objects.hash(customerID, productID, quantity, state, price);
+        return Objects.hash(orderID, customerID, productIDs, quantites, state, price);
     }
-    public Long getPrice() { return price; }
-    public void setPrice(Long price) { this.price = price;}
-    public String getState() { return state; }
-    public void setState(String state) { this.state = state; }
-    public Long getCustomerID() {return customerID;  }
-    public void setCustomerID(Long customerID) {this.customerID = customerID; }
-    public Long getProductID() {return productID; }
-    public void setProductID(Long productID) { this.productID = productID;}
-    public Long getQuantity() {return quantity;}
-    public void setQuantity(Long quantity) { this.quantity = quantity; }
 
+    public OrderBean(Long customerID, List<Long> productIDs, List<Long> quantites, String state, Long price) {
+        this.customerID = customerID;
+        this.productIDs = productIDs;
+        this.quantites = quantites;
+        this.state = state;
+        this.price = price;
+    }
+
+    public Long getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(Long customerID) {
+        this.customerID = customerID;
+    }
+
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
 }
 
 
