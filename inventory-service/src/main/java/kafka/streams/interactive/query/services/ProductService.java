183faf7b2b93b8956f65056c7e5c95a881006b71
mddarr <mddarr@gmail.com>
2020-04-25 23:17:14 -0700

    Add a product entity..

--- inventory-service/src/main/java/kafka/streams/interactive/query/services/ProductService.java
+++ inventory-service/src/main/java/kafka/streams/interactive/query/services/ProductService.java
@@ -2,6 +2,7 @@ package kafka.streams.interactive.query.services;
 
 import kafka.streams.interactive.query.bean.ProductDTO;
 import kafka.streams.interactive.query.dao.ProductRepository;
+
 import kafka.streams.interactive.query.entity.ProductEntity;
 import org.mddarr.inventory.Product;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,18 +16,18 @@ import java.util.function.BiFunction;
 @Service
 public class ProductService {
     @Autowired
-    ProductRepository productRepository;
+    ProductRepository productMongoRepository;
 
     public void addProduct(ProductDTO productDTO){
         UUID uuid =  UUID.randomUUID();
         ProductEntity product = new ProductEntity(uuid.toString(),productDTO.getName(),productDTO.getBrand(),productDTO.getPrice());
-        productRepository.save(product);
+        productMongoRepository.save(product);
     }
 
     public Optional<ProductEntity> getProduct(String id){
-        return productRepository.findById(id);
+        return productMongoRepository.findById(id);
     }
-    public void deleteProduct(String id){productRepository.deleteById(id);}
+    public void deleteProduct(String id){productMongoRepository.deleteById(id);}
 
 
 }
--- order-service/src/main/java/org/mddarr/orders/OrdersServiceApplication.java
+++ order-service/src/main/java/org/mddarr/orders/OrdersServiceApplication.java
@@ -6,12 +6,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 import javax.annotation.PostConstruct;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @ComponentScan(basePackages = "org.mddarr.orders")
 public class OrdersServiceApplication {
 
